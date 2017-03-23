1ac273b58870fbe0d0c26403513961eb1fe2279a
Stéphane Travostino <steph@combo.cc>
2017-03-23 18:51:39 +0000

    Add Object.assign polyfill

--- oddslib.js
+++ oddslib.js
@@ -1,5 +1,31 @@
 var approximateFraction = require('./approx_fraction.js');
 
+// Object.assign polyfill
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 // 1.2 - 1.0 === 0.19999999999999996
 // fixFloatError(1.2 - 1.0) === 0.2
 var fixFloatError = function(n) {
